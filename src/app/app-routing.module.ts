45afa932d291131133d33392dbce6ca255e7a772
Bnanyingi1 <58061814+Bnanyingi@users.noreply.github.com>
2023-01-24 08:51:38 +0300

    added routing to the app module inroder to access all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandPageComponent } from './modules/land-page/land-page.component';
+import { LoginPageComponent } from './modules/login-page/login-page.component';
+import { SignupPageComponent } from './modules/signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: LandPageComponent 
+  },
+  { 
+    path: 'login', 
+    component: LoginPageComponent
+  },
+  { 
+    path: 'signup', 
+    component: SignupPageComponent
+  },
+
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandPageComponent } from './modules/land-page/land-page.component';
+import { LoginPageComponent } from './modules/login-page/login-page.component';
+import { SignupPageComponent } from './modules/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandPageComponent,
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
